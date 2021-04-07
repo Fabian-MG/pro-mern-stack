51b9248053a36a6b8c6370d823dc01821e4d8d00
Fabian-MG <mirandafabian1510@gmail.com>
2021-04-07 15:41:43 -0500

    Adding issue tracker basic components

--- public/App.js
+++ public/App.js
@@ -1,11 +1,55 @@
 "use strict";
 
-var continents = ['Africa', 'America', 'Asia', 'Australia', 'Antartida'];
-var helloContinents = Array.from(continents, function (c) {
-  return "Hello ".concat(c, "!");
-});
-var message = helloContinents.join(' ');
-var element = /*#__PURE__*/React.createElement("div", {
-  title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello World"), message);
-ReactDOM.render(element, document.getElementById('content'));
+var IssueList = function IssueList() {
+  var rowStyle = {
+    border: "1px solid silver",
+    padding: 4
+  };
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Issue Tracker"), /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueTable, null, /*#__PURE__*/React.createElement(IssueRow, {
+    rowStyle: rowStyle,
+    issue_id: 1,
+    issue_title: "Error in console when clicking Add"
+  }), /*#__PURE__*/React.createElement(IssueRow, {
+    rowStyle: rowStyle,
+    issue_id: 2,
+    issue_title: "Missing bottom border on panel"
+  })), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueAdd, null));
+};
+
+var IssueFilter = function IssueFilter() {
+  return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for the issue filter");
+};
+
+var IssueTable = function IssueTable(_ref) {
+  var children = _ref.children;
+  var rowStyle = {
+    border: "1px solid silver",
+    padding: 4
+  };
+  return /*#__PURE__*/React.createElement("table", {
+    style: {
+      borderCollapse: "collapse"
+    }
+  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", {
+    style: rowStyle
+  }, "ID"), /*#__PURE__*/React.createElement("th", {
+    style: rowStyle
+  }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, children));
+};
+
+var IssueAdd = function IssueAdd() {
+  return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for a form to add an issue.");
+};
+
+var IssueRow = function IssueRow(_ref2) {
+  var issue_id = _ref2.issue_id,
+      issue_title = _ref2.issue_title,
+      rowStyle = _ref2.rowStyle;
+  return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+    style: rowStyle
+  }, issue_id), /*#__PURE__*/React.createElement("td", {
+    style: rowStyle
+  }, issue_title));
+};
+
+ReactDOM.render( /*#__PURE__*/React.createElement(IssueList, null), document.getElementById('content'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,62 @@
-const continents = ['Africa','America','Asia','Australia','Antartida']
+const IssueList = () => {
+    const rowStyle = {border: "1px solid silver", padding: 4};
 
-const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-const message = helloContinents.join(' ');
+    return(
+        <>
+            <h1>Issue Tracker</h1>
+            <IssueFilter />
+            <hr />
+            <IssueTable>
+                <IssueRow rowStyle={rowStyle} issue_id={1}
+                    issue_title="Error in console when clicking Add" />
+                <IssueRow rowStyle={rowStyle} issue_id={2}
+                    issue_title="Missing bottom border on panel" />
+            </IssueTable>
+            <hr />
+            <IssueAdd />
+        </>
+    )
+}
 
-const element = (
-    <div title="Outer div">
-        <h1>Hello World</h1>
-        {message}
-    </div>
-)
+const IssueFilter = () => {
+    return(
+        <div>
+           This is a placeholder for the issue filter
+        </div>
+    )
+}
 
-ReactDOM.render(element, document.getElementById('content'))
+const IssueTable = ({children}) => {
+    const rowStyle = {border: "1px solid silver", padding: 4};
+
+    return(
+        <table style={{borderCollapse: "collapse"}}>
+            <thead>
+                <tr>
+                    <th style={rowStyle}>ID</th>
+                    <th style={rowStyle}>Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                {children}
+            </tbody>
+        </table>
+    )
+}
+
+const IssueAdd = () => {
+    return(
+        <div>This is a placeholder for a form to add an issue.</div>
+    )
+}
+
+const IssueRow = ({issueID, issueTitle, rowStyle}) => {
+    return (
+        <tr>
+            <td style={rowStyle}>{issueID}</td>
+            <td style={rowStyle}>{issueTitle}</td>
+        </tr>
+    )
+}
+
+ReactDOM.render(<IssueList />, document.getElementById('content'))
