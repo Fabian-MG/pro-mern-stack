411a689d8578fd85645d864077e3bd0e91ea5b49
Fabian Miranda Garcia <fabian-mg@MacBook-Air-de-Fabian.local>
2021-04-11 14:30:59 -0500

    Changing table and issue components to composing pattern

--- public/App.js
+++ public/App.js
@@ -1,55 +1,49 @@
 "use strict";
 
-var IssueList = function IssueList() {
-  var rowStyle = {
-    border: "1px solid silver",
-    padding: 4
-  };
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Issue Tracker"), /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueTable, null, /*#__PURE__*/React.createElement(IssueRow, {
-    rowStyle: rowStyle,
-    issue_id: 1,
-    issue_title: "Error in console when clicking Add"
-  }), /*#__PURE__*/React.createElement(IssueRow, {
-    rowStyle: rowStyle,
-    issue_id: 2,
-    issue_title: "Missing bottom border on panel"
-  })), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueAdd, null));
-};
+var issues = [{
+  id: 1,
+  status: 'New',
+  owner: 'Ravan',
+  effort: 5,
+  created: new Date('2018-08-15'),
+  due: undefined,
+  title: 'Error in console when clicking Add'
+}, {
+  id: 2,
+  status: 'Assigned',
+  owner: 'Eddie',
+  effort: 14,
+  created: new Date('2018-08-16'),
+  due: new Date('2018-08-30'),
+  title: 'Missing bottom border on panel'
+}];
 
 var IssueFilter = function IssueFilter() {
   return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for the issue filter");
-};
-
-var IssueTable = function IssueTable(_ref) {
-  var children = _ref.children;
-  var rowStyle = {
-    border: "1px solid silver",
-    padding: 4
-  };
-  return /*#__PURE__*/React.createElement("table", {
-    style: {
-      borderCollapse: "collapse"
-    }
-  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", {
-    style: rowStyle
-  }, "ID"), /*#__PURE__*/React.createElement("th", {
-    style: rowStyle
-  }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, children));
 };
 
 var IssueAdd = function IssueAdd() {
   return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for a form to add an issue.");
 };
 
+var IssueList = function IssueList() {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Issue Tracker"), /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueTable, null, issues.map(function (issue) {
+    return /*#__PURE__*/React.createElement(IssueRow, {
+      issue: issue
+    });
+  })), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(IssueAdd, null));
+};
+
+var IssueTable = function IssueTable(_ref) {
+  var children = _ref.children;
+  return /*#__PURE__*/React.createElement("table", {
+    className: "bordered-table"
+  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "ID"), /*#__PURE__*/React.createElement("th", null, "Status"), /*#__PURE__*/React.createElement("th", null, "Owner"), /*#__PURE__*/React.createElement("th", null, "Created"), /*#__PURE__*/React.createElement("th", null, "Effort"), /*#__PURE__*/React.createElement("th", null, "Due Date"), /*#__PURE__*/React.createElement("th", null, "Title"))), /*#__PURE__*/React.createElement("tbody", null, children));
+};
+
 var IssueRow = function IssueRow(_ref2) {
-  var issue_id = _ref2.issue_id,
-      issue_title = _ref2.issue_title,
-      rowStyle = _ref2.rowStyle;
-  return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
-    style: rowStyle
-  }, issue_id), /*#__PURE__*/React.createElement("td", {
-    style: rowStyle
-  }, issue_title));
+  var issue = _ref2.issue;
+  return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, issue.id), /*#__PURE__*/React.createElement("td", null, issue.status), /*#__PURE__*/React.createElement("td", null, issue.owner), /*#__PURE__*/React.createElement("td", null, issue.created.toDateString()), /*#__PURE__*/React.createElement("td", null, issue.effort), /*#__PURE__*/React.createElement("td", null, issue.due ? issue.due.toDateString() : ''), /*#__PURE__*/React.createElement("td", null, issue.title));
 };
 
 ReactDOM.render( /*#__PURE__*/React.createElement(IssueList, null), document.getElementById('content'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-const IssueList = () => {
-    const rowStyle = {border: "1px solid silver", padding: 4};
-
-    return(
-        <>
-            <h1>Issue Tracker</h1>
-            <IssueFilter />
-            <hr />
-            <IssueTable>
-                <IssueRow rowStyle={rowStyle} issue_id={1}
-                    issue_title="Error in console when clicking Add" />
-                <IssueRow rowStyle={rowStyle} issue_id={2}
-                    issue_title="Missing bottom border on panel" />
-            </IssueTable>
-            <hr />
-            <IssueAdd />
-        </>
-    )
-}
+const issues = [
+  {
+      id: 1, status: 'New', owner: 'Ravan', effort: 5,
+      created: new Date('2018-08-15'), due: undefined,
+      title: 'Error in console when clicking Add',
+  },
+  {
+      id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
+      created: new Date('2018-08-16'), due: new Date('2018-08-30'),
+      title: 'Missing bottom border on panel',
+  } 
+];
 
 const IssueFilter = () => {
     return(
@@ -26,15 +19,41 @@ const IssueFilter = () => {
     )
 }
 
-const IssueTable = ({children}) => {
-    const rowStyle = {border: "1px solid silver", padding: 4};
-
+const IssueAdd = () => {
     return(
-        <table style={{borderCollapse: "collapse"}}>
+        <div>This is a placeholder for a form to add an issue.</div>
+    )
+}
+
+const IssueList = () => {
+    return(
+        <>
+            <h1>Issue Tracker</h1>
+            <IssueFilter />
+            <hr />
+            <IssueTable>
+            {issues.map((issue) => 
+                <IssueRow issue={issue} />
+            )}
+            </IssueTable>
+            <hr />
+            <IssueAdd />
+        </>
+    )
+}
+
+const IssueTable = ({children}) => {
+    return(
+        <table className="bordered-table">
             <thead>
                 <tr>
-                    <th style={rowStyle}>ID</th>
-                    <th style={rowStyle}>Title</th>
+                    <th>ID</th>
+                    <th>Status</th>
+                    <th>Owner</th>
+                    <th>Created</th>
+                    <th>Effort</th>
+                    <th>Due Date</th>
+                    <th>Title</th>
                 </tr>
             </thead>
             <tbody>
@@ -44,17 +63,16 @@ const IssueTable = ({children}) => {
     )
 }
 
-const IssueAdd = () => {
-    return(
-        <div>This is a placeholder for a form to add an issue.</div>
-    )
-}
-
-const IssueRow = ({issueID, issueTitle, rowStyle}) => {
+const IssueRow = ({issue}) => {
     return (
         <tr>
-            <td style={rowStyle}>{issueID}</td>
-            <td style={rowStyle}>{issueTitle}</td>
+            <td>{issue.id}</td>
+            <td>{issue.status}</td>
+            <td>{issue.owner}</td>
+            <td>{issue.created.toDateString()}</td>
+            <td>{issue.effort}</td>
+            <td>{issue.due ? issue.due.toDateString() : ''}</td>
+            <td>{issue.title}</td>
         </tr>
     )
 }
