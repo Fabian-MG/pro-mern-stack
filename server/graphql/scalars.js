626ef3fb5eccd893a0ca934c05a019ea8e30e837
Fabian-MG <mirandafabian1510@gmail.com>
2021-05-20 15:11:42 -0500

    Changing scalar types to graphql folder

---
+++ server/graphql/scalars.js
@@ -0,0 +1,22 @@
+const { Kind } = require("graphql/language");
+const { GraphQLScalarType } = require("graphql");
+
+const GraphQLDate = new GraphQLScalarType({
+  name: "GraphQLDate",
+  description: "A date type in GraphQL as a scalar",
+  serialize: (value) => value.toISOString(),
+  parseLiteral: (ast) => {
+    if (ast.kind == Kind.STRING) {
+      const value = new Date(ast.value);
+      return isNaN(value) ? undefined : value;
+    }
+  },
+  parseValue: (value) => {
+    const dateValue = new Date(value);
+    return isNaN(dateValue) ? undefined : dateValue;
+  },
+});
+
+module.exports = {
+  GraphQLDate,
+};
